119ff3c0a8478ace3b0925ebfbd4f447408bf498
Kelvin <@kkituni@gmail.comgit config --global user.email @kkituni@gmail.com>
2019-03-31 23:29:33 +0300

    Added conditional statements for results

--- js/responses.js
+++ js/responses.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
     $('#results form').submit(function(e) {
       e.preventDefault();
+      const form = $(this);
+      const score = form.find(':checked[correct-choice]').length;
+      const scorePercent = parseFloat(score/5*100);
+      if(scorePercent >= 80 ) {
+        document.getElementById('myComment').innerHTML = 'EXCELLENT!!';
+      } else if(scorePercent <80 && scorePercent >= 50 ) {
+        document.getElementById('myComment').innerHTML = 'FAIR';
+      }else {
+        document.getElementById('myComment').innerHTML = 'POOR. RETAKE THE TEST';
+      }
+      });
+
+
+      });
