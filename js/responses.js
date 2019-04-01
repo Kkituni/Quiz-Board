204c0e94f19d8757f7d5263db288a735fdaad3b8
Kelvin <@kkituni@gmail.comgit config --global user.email @kkituni@gmail.com>
2019-04-01 15:14:16 +0300

    Changes to index and js files

--- js/responses.js
+++ js/responses.js
@@ -6,26 +6,17 @@ $(document).ready(function() {
       const score = form.find(':checked[correct-choice]').length;
       const scorePercent = parseFloat(score/5*100);
       if(scorePercent >= 80 ) {
-<<<<<<< HEAD
-        document.getElementById('myComment').innerHTML = "Your score is  ".concat(scorePercent);
-      }
-      else if(scorePercent <80 && scorePercent >= 50 ) {
-        document.getElementById('myComment').innerHTML = "Your score is  ".concat(scorePercent);
-      }
-      else {
-        document.getElementById('myComment').innerHTML = "Your score is  ".concat(scorePercent);
-=======
-        document.getElementById('myComment').innerHTML = "Your score is ".concat(scorePercent); + "EXCELLENT!!";
+        document.getElementById('myComment').innerHTML = "Your score is : ".concat(scorePercent);
       } else if(scorePercent <80 && scorePercent >= 50 ) {
-        document.getElementById('myComment').innerHTML = "Your score is ".concat(scorePercent); + "FAIR";
+        document.getElementById('myComment').innerHTML = "Your score is : ".concat(scorePercent);
       }else {
-        document.getElementById('myComment').innerHTML = "Your score is ".concat(scorePercent); + "POOR. RETAKE THE TEST";
->>>>>>> df550891273f6991a8d21e06947e6ba164a37e14
+        document.getElementById('myComment').innerHTML = "Your score is : ".concat(scorePercent);
       }
     });
 
 
   });
+
 
   function clearInputs(){
     $('#results form').trigger('reset');
