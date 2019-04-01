6003b67a82f92381f2f55562476275e083459bbd
Kelvin <@kkituni@gmail.comgit config --global user.email @kkituni@gmail.com>
2019-04-01 12:16:59 +0300

    Modified comments

--- js/responses.js
+++ js/responses.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
     $('#results form').submit(function(e) {
+
       e.preventDefault();
       const form = $(this);
       const score = form.find(':checked[correct-choice]').length;
       const scorePercent = parseFloat(score/5*100);
       if(scorePercent >= 80 ) {
-        document.getElementById('myComment').innerHTML = "Your score is " + .concat(scorePercent); + " EXCELLENT!!";
+        document.getElementById('myComment').innerHTML = 'EXCELLENT!!';
       } else if(scorePercent <80 && scorePercent >= 50 ) {
-        document.getElementById('myComment').innerHTML = "Your score is ".concat(scorePercent); + "FAIR";
+        document.getElementById('myComment').innerHTML = 'FAIR';
       }else {
-        document.getElementById('myComment').innerHTML = "Your score is ".concat(scorePercent); + "POOR. RETAKE THE TEST";
+        document.getElementById('myComment').innerHTML = 'POOR. RETAKE THE TEST';
       }
     });
 
